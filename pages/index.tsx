09ef3b64278ce4b26adbac1fc9503edfd5531867
Alan Quach <1633736+alanmquach@users.noreply.github.com>
2022-04-13 23:14:00 -0700

    feat(core): DocPad.coffee => Next.ts

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from 'next'
+
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+import { remark } from 'remark'
+import html from 'remark-html'
+import Link from 'next/link'
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+export async function getStaticProps() {
+  const fullPath = path.join(postsDirectory, `2014-01-01-new-year-new-blog.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents)
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  // Combine the data with the id and contentHtml
+  return {
+    props: {
+      contentHtml,
+      ...matterResult.data
+    }
+  }
+}
+const Home: NextPage<{ contentHtml: string, title: string }> = ({ contentHtml, title }) => {
+  return (
+    <div className="content">
+      <div className="post">
+        <h1><Link href="/2014/01/02/new-year-new-blog">{title}</Link></h1>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
